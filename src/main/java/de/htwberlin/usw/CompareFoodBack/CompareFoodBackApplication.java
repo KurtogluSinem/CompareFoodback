84d35fff61532c996855a468432c9a478b6353ed
KurtogluSinem <krtgl.sinem2519@gmail.com>
2023-05-28 14:46:11 +0200

    repo

---
+++ src/main/java/de/htwberlin/usw/CompareFoodBack/CompareFoodBackApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.usw.CompareFoodBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompareFoodBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompareFoodBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/usw/CompareFoodBack/CompareFoodBackApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.usw.CompareFoodBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompareFoodBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
